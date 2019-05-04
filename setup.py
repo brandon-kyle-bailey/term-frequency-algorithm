dd89538e4eb5a0062012d1c3a3b01e30d3bc6807
brandonsgitrepo <brandonkylebailey@outlook.com>
2019-05-04 23:56:07 +0100

    creation of setup file

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+def readme():
+    with open('docs/README.md') as f:
+        return f.read()
+
+setup(name='term-frequency-algorithm',
+      version='0.1',
+      description='The funniest joke in the world',
+      long_description=readme(),
+      classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Term Frequency :: Algorithms',
+      ],
+      keywords='technical test',
+      url='None',
+      author='Brandon Bailey',
+      author_email='brandonkylebailey@outlook.com',
+      license='None',
+      packages=['app'],
+      install_requires=[
+          'Python>=3.7.3',
+      ],
+      include_package_data=True,
+      zip_safe=False)
