930735fc314f3abffb79ea4f36d12394423884df
brandonsgitrepo <brandonkylebailey@outlook.com>
2019-05-04 22:55:42 +0100

    PEP8 adherance, syntax cleanup, execution time tracking added

--- app/search.py
+++ app/search.py
@@ -4,101 +4,164 @@ import os
 import sys
 import json
 import math
-
+import time
 
 
 class Search:
 
-	def __init__(self):
-		self.search_query = sys.argv[1:]
-		self.main()
+    def __init__(self):
 
+        self.search_query = sys.argv[1:]
+        self.main()
 
-	def get_data_set(self, filename):
+    def get_data_set(self, filename):
+        """ Retrieves json object from json file specifiec
 
-		json_file = os.path.join(os.getcwd(), filename)
+        args:
+            filename : name of file
 
-		with open(json_file, encoding="utf8") as data_set:
-			data = json.load(data_set)
+        returns:
+            data : json data object
+        """
 
-		return data
+        root = os.getcwd()
 
-	def get_term_frequency(self, words, entry):
+        json_file = os.path.join(root, filename)
 
-		words = [w.lower() for w in words]
-		entry = ' '.join([e.lower() for e in entry.split(' ')])
+        if not os.path.exists(json_file):
+            raise RuntimeError("Json file can not be found. Is it in root?")
 
-		word_dict = dict.fromkeys(words, 0)
+        # open json file and explicity define utf8 encoding as
+        # explicit defining is needed if running on windows
+        with open(json_file, encoding="utf8") as data_set:
+            data = json.load(data_set)
 
-		for word in words:
-			word_dict[word] = entry.count(word)
+        return data
 
-		out_dict = dict()
-		counter = len(entry)
+    def get_term_frequency(self, words, entry):
+        """algorithm to return a dictionary of search items
+        and their corresponding frequency score against entry
+        data.
 
-		for word, count in word_dict.items():
-			out_dict[word] = count/float(counter)
+        args:
+            words : search item words
+            entry : speicif entry from json object
 
-		return out_dict
+        returns:
+            frequency_dict : dictionary of word/score pairs
+        """
 
-	def get_frequency(self, search_words, entry, name):
+        # clean data for both search items and json entry data
+        cleaned_words = [word.lower() for word in words]
+        cleaned_entry = ' '.join([item.lower() for item in entry.split(' ')])
 
-		entry_data = entry.get(name, None)
-		if not entry_data:
-			raise ValueError("entry_data for 'name' field is empty.")
+        # form dictionary template using cleaned search items
+        word_dict = dict.fromkeys(cleaned_words, 0)
 
-		frequency_value = self.get_term_frequency(search_words, entry_data)
-		# print(frequency_value)
+        # append word frequency value with its count presense in json
+        # data object
+        for word in cleaned_words:
+            word_dict[word] = cleaned_entry.count(word)
 
-		return frequency_value
+        frequency_dict = dict()
+        entry_length = len(cleaned_entry)
 
-	def get_top_ranking(self, json_data, level=10):
-		"""Used to retrieve N number of highest ranks.
-		default is 10
+        # iterate over words in dictionary and devide its
+        # count value by the length of the entry to create
+        # an average frequency value
+        for word, count in word_dict.items():
+            frequency_dict[word] = count/float(entry_length)
 
-		args:
-			json_data : json_data for rank retrieval
-			level : number of returned highest ranks
+        return frequency_dict
 
-		returns:
-			top_rank_list : list of top ranked items
-		"""
+    def get_frequency(self, search_words, entry, name):
+        """Retrieves the specific key value from the entry and
+        returns the frequency value of the search items
 
-		temp_data = json_data
+        args:
+            search_words : list of search items
+            entry : specific entry from json data
+            name : name of key to retrieve value of
 
-		top_rank_list = list()
-		for i in range(level):
-			top_rank_list.append(max(temp_data, key=lambda x: x['score']))
-			temp_data.remove(max(temp_data, key=lambda x: x['score']))
+        returns:
+            frequency_value : dictionary value of words and frequencies
+        """
 
-		return top_rank_list		 
+        entry_data = entry.get(name, None)
 
-	def main(self):
+        # if entry key can not be retrieved, data is corrupt and we
+        # should exit and notify the user
+        if not entry_data:
+            raise ValueError("entry_data for 'name' field is empty.")
 
-		filename = "search_dataset.json"
-		json_data = self.get_data_set(filename)
+        frequency_value = self.get_term_frequency(search_words, entry_data)
 
-		search_words = self.search_query[0].split(' ')
-		
-		for entry in json_data:
+        return frequency_value
 
-			name_frequency_value = self.get_frequency(search_words, entry, "name")
-			brand_frequency_value = self.get_frequency(search_words, entry, "brand")
+    def get_top_ranking(self, json_data, level=10):
+        """Used to retrieve N number of highest ranks.
+        default is 10
 
-			out_dict = name_frequency_value.copy()
-			for key, value in name_frequency_value.items():
-				out_dict[key] += brand_frequency_value[key]
+        args:
+            json_data : json_data for rank retrieval
+            level : number of returned highest ranks
 
-			entry['score'] = sum(out_dict.values())
+        returns:
+            top_rank_list : list of top ranked items
+        """
 
+        # create a temporary data object for iteration
+        temp_data = json_data
 
-		top_ranking = self.get_top_ranking(json_data, level=10)
+        # iterate through the temporary object using the level value as
+        # a range and retrieve the highest scoring entry and
+        top_rank_list = list()
+        for i in range(level):
 
-                for num, i in enumerate(top_ranking): 
-                    print(num, i)
+            top_rank_list.append(max(temp_data,
+                                    key=lambda x: x['score']))
+
+            temp_data.remove(max(temp_data,
+                                    key=lambda x: x['score']))
+
+        return top_rank_list
+
+    def main(self):
+
+        filename = "search_dataset.json"
+        json_data = self.get_data_set(filename)
+
+        search_words = self.search_query[0].split(' ')
+
+        # for each entry in json data, retrieve the frequency of
+        # name and brand objects, flattern the frequency values.
+        # create a score key for the entry and append current score.
+        for entry in json_data:
+
+            name_frequency_value = self.get_frequency(search_words,
+                                                        entry, "name")
+
+            brand_frequency_value = self.get_frequency(search_words,
+                                                        entry, "brand")
+
+            # here we flattern the two frequency dictionaries to get
+            # a single dictionary of words and their frequency values.
+            out_dict = name_frequency_value.copy()
+            for key, value in name_frequency_value.items():
+                out_dict[key] += brand_frequency_value[key]
+
+            entry['score'] = sum(out_dict.values())
+
+        # return top ranking entries
+        top_ranking = self.get_top_ranking(json_data, level=10)
+        for tally, result in enumerate(top_ranking):
+            print(tally, result)
 
 
 if __name__ == "__main__":
 
+    start = time.time()
+
     Search()
 
+    print(f"Executed OK --- {(time.time() - start)} seconds ---")
